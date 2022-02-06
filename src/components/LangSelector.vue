92d82a8e548a155fe34037b82e835e582308ebb3
Jeison Daza <derera199@gmail.com>
2022-02-06 08:53:59 -0400

    feat: add select languages styles

--- src/components/LangSelector.vue
+++ src/components/LangSelector.vue
@@ -2,16 +2,61 @@
 import { useI18n } from "vue-i18n";
 
 const { t, locale } = useI18n();
-const langs = ["en", "es"];
+const langs = ["en", "es", "fr"];
 </script>
 
 <template>
-   <section>
-      <label for="locale">{{ t("label") }}</label>
-      <select v-model="locale">
-         <option v-for="(lang, i) in langs" :key="`lang-${i}`" :value="lang">
+   <section class="locale_conatainer">
+      <label class="locale_label" for="locale">{{ t("label") }}</label>
+      <select class="locale_select" v-model="locale" id="locale">
+         <option
+            class="locale_opt"
+            v-for="(lang, i) in langs"
+            :key="`lang-${i}`"
+            :value="lang"
+         >
             {{ lang.toUpperCase() }}
          </option>
       </select>
    </section>
 </template>
+
+<style>
+.locale_conatainer {
+   display: flex;
+   flex-direction: column;
+   align-items: flex-end;
+   gap: 5px;
+}
+.locale_label {
+   color: white;
+}
+.locale_select {
+   border: none;
+   padding: 4px;
+   border-radius: 5px;
+   color: var(--accent-color);
+   background-color: var(--light-bg-color);
+   width: 50px;
+   font-weight: bold;
+}
+.locale_select:focus {
+   outline: none;
+}
+.locale_opt:checked {
+   color: white;
+   font-weight: bold;
+   background-color: var(--primary-color);
+}
+@media screen and (min-width: 450px) {
+   .locale_conatainer {
+      flex-direction: row;
+      align-items: center;
+   }
+}
+@media screen and (min-width: 1000px) {
+   .locale_opt {
+      font-size: 15px;
+   }
+}
+</style>
